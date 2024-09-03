cb64e56cc15c84df905bbdfa4b4babd29694c524
priyanka <priyankajadhav3112001@gmail.com>
2024-09-03 17:04:08 +0530

    is adult function added

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const age = 24
-const name = "priyanka"
-const dob = "31/01/2001"
+const age = 24;
+const name = "priyanka";
+const dob = "31/01/2001";
+
+
+const isadult = () => {
+    return age >= 24;
+}
